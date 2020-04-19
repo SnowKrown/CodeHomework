5b3ab27a324830707e61f9b6149210d38c8b21d2
Cristian Montenegro <crisdarmon@gmail.com>
2020-04-19 13:26:29 -0500

    Se agrego el ejercicio 5.

---
+++ Taller Guía 2/Ejercicio5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float a, b = 0;
+	
+	cout<<"Ingrese el numero a dividir: ";
+	cin>>a;
+	
+	while (b == 0)
+	{
+		cout<<"Ingrese el divisor: ";
+		cin>>b;
+		
+		if (b == 0)
+			cout<<"El divisor no puede ser cero."<<endl<<endl;
+	}
+	
+	cout<<"El resultado de dividir "<<a<<" entre "<<b<<" es: "<<(a/b);
+}
