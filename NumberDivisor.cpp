61df3be0f8cc1927eabad068354f18c29e6e96e1
Cristian Montenegro <crisdarmon@gmail.com>
2020-04-19 11:36:09 -0500

    Se agrego el la funcionalidad de dividir un número entre 100.

--- NumberDivisor.cpp
+++ NumberDivisor.cpp
@@ -1,6 +1,13 @@
-#include<conio.h>
+using namespace std;
+#include<iostream>
 
-void main()
+int main()
 {
+	float number = 0;
+	cout<<"Ingrese un numero: ";
+	cin>>number;
 	
+	float result = number / 100;
+	cout<<"El resultado de dividir "<<number<<" entre 100 es: "<<result;
+	return 0;
 }
