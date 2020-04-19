96857f15eb163f006939705581bb6573a8d9246c
Cristian Montenegro <crisdarmon@gmail.com>
2020-04-19 17:54:50 -0500

    Se agregaron los ejercicios 8 y 9

---
+++ Taller Guía 2/Ejercicio8.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+char* getMonthName(int month);
+int getMonthDays(int month);
+
+int main()
+{
+	int m;
+	cout<<"Ingrese el mes: ";
+	cin>>m;
+	
+	if (m >= 1 && m <= 12)
+		cout<<"El mes de "<<getMonthName(m)<<" tiene "<<getMonthDays(m)<<" dias."<<endl;
+	else
+		cout<<"El mes "<<m<<" no es valido.";
+}
+
+char* getMonthName(int month)
+{
+	switch(month)
+	{
+		case 1: return "Enero";
+		case 2: return "Febrero";
+		case 3: return "Marzo";
+		case 4: return "Abril";
+		case 5: return "Mayo"; 
+		case 6: return "Junio";
+		case 7: return "Julio";
+		case 8: return "Agosto";
+		case 9: return "Septiembre";
+		case 10: return "Octubre";
+		case 11: return "Noviembre";
+		case 12: return "Diciembre";
+		default: return "Mes invalido";
+	}
+}
+
+int getMonthDays(int month)
+{
+	switch(month)
+	{
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12: return 31;
+		case 4: case 6: case 9: case 11: return 30;
+		default: return 28;
+	}
+}
+
---
+++ Taller Guía 2/Ejercicio9.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+char* getDayNumber(int day);
+
+int main()
+{
+	int day;
+	
+	while (day < 1 || day > 7)
+	{
+		cout<<"Escriba el dia (del 1 al 7): ";
+		cin>>day;
+		
+		if (day > 0 && day < 8)
+			cout<<"El dia es "<<getDayNumber(day);
+		else
+			cout<<"El dia "<<day<<" es invalido."<<endl<<endl;
+	}
+}
+
+char* getDayNumber(int day)
+{
+	switch(day)
+	{
+		case 1: return "Lunes";
+		case 2: return "Martes";
+		case 3: return "Miercoles";
+		case 4: return "Jueves";
+		case 5: return "Viernes";
+		case 6: return "Sabado";
+		default: return "Domingo";
+	}
+}
