a16c896325561806bb13d5c8b0b2898d43d685d9
Cristian Montenegro <crisdarmon@gmail.com>
2020-04-19 19:57:33 -0500

    Se agregaron los ejercicios 15 y 16.

---
+++ Taller Guía 2/Ejercicio15.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int cant, tot;
+	cout<<"Ingrese la cantidad de copias a sacar: ";
+	cin>>cant;
+	tot = 100 * cant;
+	
+	if (cant >= 100 && cant <= 200)
+		tot -= tot * 0.12;
+	else if (cant > 200 && cant <= 400)
+		tot -= tot * 0.15;
+	else if (cant > 400)
+		tot -= tot * 0.18;
+		
+	cout<<"El total a pagar es: "<<tot;
+}
---
+++ Taller Guía 2/Ejercicio16.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+void printNumbers (bool drawPairs);
+
+int main()
+{
+	cout<<"IMPARES:"<<endl;
+	printNumbers(false);
+	cout<<endl;
+	cout<<"PARES:"<<endl;
+	printNumbers(true);
+}
+
+void printNumbers(bool drawPairs)
+{
+	for(int i = 1; i < 100; i++)
+	{
+		bool isPair = i % 2 == 0;
+		
+		if (drawPairs && isPair)
+			cout<<"P: "<<i<<endl;
+			
+		if (!drawPairs && !isPair)
+			cout<<"I: "<<i<<endl;
+	}
+}
