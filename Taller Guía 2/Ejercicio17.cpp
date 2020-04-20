44233e0cd4fffa8d076b01d5f738593c7e16a766
Cristian Montenegro <crisdarmon@gmail.com>
2020-04-19 20:18:09 -0500

    Se agregaron los ejercicios 17 y 18.

---
+++ Taller Guía 2/Ejercicio17.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double a = 0, b = 1, x = 0;
+	
+	for (int i = 0; i < 100; i++)
+	{
+		x = a;
+		a = b;
+		b = x + a;
+		
+		cout<<"F: "<<a<<endl;
+	}
+}
---
+++ Taller Guía 2/Ejercicio18.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, res;
+	
+	cout<<"Ingrese el valor de N: ";
+	cin>>n;
+	res = n;
+	
+	for (int i = 1; i < n; i++)
+		res *= i;
+	
+	cout<<"El factorial de "<<n<<" es: "<<res;
+}
