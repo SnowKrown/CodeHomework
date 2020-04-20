fb5ac0430da879e3f1206cfe9307e4ce2c206b76
Cristian Montenegro <crisdarmon@gmail.com>
2020-04-19 19:31:47 -0500

    Se agregaron los ejercicios 13 y 14.

---
+++ Taller Guía 2/Ejercicio13.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+float getResult(float a, float b, int operation);
+
+int main()
+{
+	float a, b; 
+	int opt;
+	cout<<"Ingrese el primer numero: ";
+	cin>>a;
+	cout<<"Ingrese el segundo numero: ";
+	cin>>b;
+	cout<<endl<<endl;
+	cout<<"MENU"<<endl<<"1. Sumar"<<endl<<"2. Restar"<<endl<<"3. Multiplicar"<<endl<<"4. Dividir"<<endl;
+	
+	cout<<"Elija la opcion deseada: ";
+	cin>>opt;
+	
+	if (opt < 1 || opt > 4)
+		cout<<"La opcion escogida no es valida."<<endl<<endl;
+	else
+		cout<<"El resultado de la operacion es: "<<getResult(a, b, opt)<<endl<<endl;
+}
+
+float getResult(float a, float b, int operation)
+{
+	switch(operation)
+	{
+		case 1: return a + b;
+		case 2: return a - b;
+		case 3: return a * b;
+		case 4: return a / b;
+		default: return 0;
+	}
+}
---
+++ Taller Guía 2/Ejercicio14.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int h, m, n;
+	
+	cout<<"Ingrese la cantidad de hombres: ";
+	cin>>h;
+	cout<<"Ingrese la cantidad de mujeres: ";
+	cin>>m;
+	cout<<"Ingrese la cantidad de ninos: ";
+	cin>>n;
+	
+	float t, hPct, mPct, nPct;
+	t = (h + m + n);
+	hPct = (h / t) * 100;
+	mPct = (m / t) * 100;
+	nPct = (n / t) * 100;
+	
+	cout<<"El porcentaje de personas es:"<<endl<<"Hombres: "<<hPct<<"%"<<endl<<"Mujeres: "<<mPct<<"%"<<endl<<"Ninos: "<<nPct<<"%"<<endl;
+}
